9dbedfc7605a6aab138caddcbbc07588358010ba
Daniel Moura <daniel.tecnicop@gmail.com>
2024-11-08 16:06:25 -0400

    Criado componente movimento caixa

---
+++ src/app/component/movimento-caixa/movimento-caixa-create/movimento-caixa-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movimento-caixa-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './movimento-caixa-create.component.html',
+  styleUrl: './movimento-caixa-create.component.css'
+})
+export class MovimentoCaixaCreateComponent {
+
+}
